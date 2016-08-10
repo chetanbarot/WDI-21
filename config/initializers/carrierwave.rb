66c116117441ca88b6f6a32f9d58271ba038650e
chetan barot <chetanbarot@hotmail.com>
2016-08-10 16:45:32 +0100

    Moved carrierwave config, fixed env var names

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,10 @@ carrierWave. configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id :ENV['AWS_ACCESS_KEY'],
-      aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+      aws_access_key_id: ENV['aws_access_key'],
+      aws_secret_access_key: ENV['aws_secret_key'],
       region: 'eu-west-1'
     }
-    config.fog_directory = ENV['AWS_BUCKET_NAME']
+    config.fog_directory = ENV['aws_bucket_name']
   end
 end
