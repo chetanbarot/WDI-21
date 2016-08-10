39ae4325ae816e938e0092bdc367a9e426c43849
chetan barot <chetanbarot@hotmail.com>
2016-08-10 17:07:07 +0100

    Updated bucket region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['aws_access_key'],
       aws_secret_access_key: ENV['aws_secret_key'],
-      region: 'eu-west-1'
+      region: 'us-west-2'
     }
     config.fog_directory = ENV['aws_bucket_name']
   end
