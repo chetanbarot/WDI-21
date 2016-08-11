b27cac3ca8acce66aa846277b3caebde3c78fe65
chetan barot <chetanbarot@hotmail.com>
2016-08-11 11:43:32 +0100

    Removed passwords for ENV vars

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,10 @@ CarrierWave.configure do |config|
   else
     config.storage = :aws
     config.aws_acl = 'public-read'
-    config.aws_bucket = "projectcookbook"
+    config.aws_bucket = ENV['AWS_BUCKET_NAME']
     config.aws_credentials = {
-      access_key_id: "AKIAIF4IYW4X5ZG7T6KA",
-      secret_access_key: "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",
+      access_key_id: ENV['AWS_ACCESS_KEY'],
+      secret_access_key: ENV['AWS_SECRET_KEY'],
       region: 'eu-west-1'
     }
   end
